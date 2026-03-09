5e62bbd2e6984d7cda8d5aed32b77c0643bfc7a9
Giro <maks07072004@gmail.com>
2026-03-09 21:35:51 +0700

    Configure project for Ktor, napier, kotlinx-serialization

---
+++ composeApp/src/androidMain/kotlin/com/example/kts_android_kmp/network/KtorEngineFactory.android.kt
@@ -0,0 +1,9 @@
+package com.example.kts_android_kmp.network
+
+import io.ktor.client.engine.HttpClientEngine
+import io.ktor.client.engine.okhttp.OkHttp
+
+internal actual object KtorEngineFactory {
+    actual fun create(): HttpClientEngine = OkHttp.create()
+}
+
